bef96cfbba2e167991d0a2aaec4e45a400058894
Moritz Pleintinger <mpleinti@lap79140.mpe.mpg.de>
2019-01-23 10:34:35 +0100

    Added random string to spi_frame

--- pyspi/spi_frame.py
+++ pyspi/spi_frame.py
@@ -5,7 +5,8 @@ from astropy.coordinates import BaseCoordinateFrame, Attribute, RepresentationMa
 from astropy.coordinates import frame_transform_graph, spherical_to_cartesian
     
 from pyspi.spi_pointing import _construct_sc_matrix
-# Random
+
+# alfkarewölgjnöserjvnbs
 
 class SPIFrame(BaseCoordinateFrame):
     """
