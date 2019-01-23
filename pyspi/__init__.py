c1f1fe6a849652d55aaf5a266f39b756b7b80b9e
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-01-23 16:04:13 +0100

    note sure what I changed

--- pyspi/__init__.py
+++ pyspi/__init__.py
@@ -1,2 +1,9 @@
 from pyspi.spi_response import SPIResponse
 from pyspi.SPILike import SPILike
+
+
+from IPython.display import display, Video
+
+
+
+display(Video('https://br-i.akamaihd.net/i/MUJIuUOVBwQIbtC2uKJDM6OhuLnC_2rc5K1S/_AJS/9-rc5ygy/4ff8be83-80eb-45e6-bf4c-2b43e2344759_,0,A,B,E,C,X,.mp4.csmil/master.m3u8?__b__=200'))
