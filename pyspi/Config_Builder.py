894b04a25281d8a6460f0bbd31b2819443135ab3
Björn Biltzinger <bjoernbiltzinger@gmx.de>
2020-03-02 17:18:00 +0100

    Added config for constant source

--- pyspi/Config_Builder.py
+++ pyspi/Config_Builder.py
@@ -16,7 +16,21 @@ default_GRB = {'Unique_analysis_name': None,
                'Energy_binned': True,
                'Ebounds': np.logspace(np.log10(20), np.log10(8000), 30).tolist(),
                'Use_only_photopeak': False}
-            
+
+
+default_CS = {'Unique_analysis_name': None,
+               'Special_analysis': 'Constant_Source',
+               'Event_types': ['single'],
+               'Detectors_to_use': 'All',
+               'emin': '20',
+               'emax': '8000',
+               'Simulate': None,
+               'Bkg_estimation': 'Polynominal',
+               'Pointings':np.array(['010300010010']).tolist(),
+               'Energy_binned': True,
+               'Ebounds': np.logspace(np.log10(20), np.log10(8000), 30).tolist(),
+               'Use_only_photopeak': False}
+
            
 
 class basic_config_builder_GRB(object):
@@ -70,3 +84,50 @@ class basic_config_builder_GRB(object):
 #                         'ra': 10}
 #class Config_Builder_Point_Sources(object):
         
+class basic_config_builder_Constant_Source(object):
+    """
+    Class to build the config yaml file used in to specify which analysis should be done.
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Init the config file for a GRB analysis
+        """
+        self._config = default_CS
+        for key, value in kwargs.iteritems():
+            assert key in self._config.keys(),'Please use valid key. Only {} are available.'.format(self._config.keys())
+            self._config[key] = value
+
+        self._config_savepath = './config.yaml'
+        with open(self._config_savepath, 'w') as file:
+            yaml.dump(self._config, file)
+            
+    def change_config(self, **kwargs):
+        """
+        Change the current config
+        :param kwargs: dict with the keywords and new values
+        :return:
+        """
+        for key, value in kwargs.iteritems():
+            assert key in self._config.keys(), 'Please use valid key. Only {} are available.'.format(self._config.keys())
+            if key=='Ebounds' or 'Pointings':
+                value=value.tolist()
+            self._config[key] = value
+        with open(self._config_savepath, 'w') as file:
+            yaml.dump(self._config, file)
+            
+    def display(self):
+        """
+        Display current config file
+        :return:
+        """
+        print(yaml.dump(self._config))
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def save_path(self):
+        return self._config_savepath
+
