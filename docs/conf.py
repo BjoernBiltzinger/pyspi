dc8dbb5e61fa97080bf4e0971e3f42a2b1b037a5
grburgess <jmichaelburgess@gmail.com>
2021-07-06 13:59:14 +0200

    whatever

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,8 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
