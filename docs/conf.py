7a817165de4b68bd0b2209986481d5a8a882979a
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2021-10-28 12:33:35 +0200

    try to fix api

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,8 @@ def run_apidoc(app):
     better_apidoc.main(
         [
             "better-apidoc",
-            # "-t",
-            # str(docs / "_templates"),
+            "-t",
+            str(DOCS / "_templates"),
             "--force",
             "--no-toc",
             "--separate",
